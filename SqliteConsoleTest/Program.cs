c4387b656ecad51693c9f4bbac5956e5997bcf6a
Jorge Cabot <jcabot@jorgecabot.com>
2018-08-15 17:29:39 -0400

    Added simple SqlLite test project.

---
+++ SqliteConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace SqliteConsoleTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                await connection.OpenAsync();
+
+                var command = new SqliteCommand("SELECT 1;", connection);
+                var result = (long) await command.ExecuteScalarAsync();
+
+                Console.WriteLine($"Command output: {result}");
+            }
+        }
+    }
+}
