2126dc06d3e4340bbc53b50b4f456ef9dcab1b1d
Jorge Cabot <jcabot@jorgecabot.com>
2018-08-26 10:36:13 -0400

    Added tables and entries to data fixture.

--- CreateTablesTest/SqliteScmTest/SampleScmDataFixture.cs
+++ CreateTablesTest/SqliteScmTest/SampleScmDataFixture.cs
@@ -20,6 +20,44 @@ namespace SqliteScmTest
             FOREIGN KEY(PartTypeId) REFERENCES PartType(Id)
             )";
 
+        private const string PartCommandTable =
+        @"CREATE TABLE PartCommand(
+            Id INTEGER PRIMARY KEY,
+            Count INTEGER NOT NULL,
+            PartTypeId INTEGER NOT NULL,
+            Command VARCHAR(50) NOT NULL,
+            FOREIGN KEY(PartTypeId) REFERENCES PartType(Id)
+          );";
+
+        private const string SupplierTable =
+            @"CREATE TABLE Supplier(
+                Id INTEGER PRIMARY KEY,
+                Name VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL,
+                PartTypeId INTEGER NOT NULL,
+                FOREIGN KEY(PartTypeId) REFERENCES PartType(Id)
+            );";
+
+        private const string OrderTable =
+            @"CREATE TABLE [Order](
+                Id INTEGER PRIMARY KEY,
+                SupplierId INTEGER NOT NULL,
+                PartTypeId INTEGER NOT NULL,
+                PartCount INTEGER NOT NULL,
+                PlacedDate DATETIME NOT NULL,
+                FulfilledDate DATETIME,
+                FOREIGN KEY(SupplierId) REFERENCES Supplier(Id),
+                FOREIGN KEY(PartTypeId) REFERENCES PartType(Id)
+            );";
+
+        private const string SendEmailCommandTable =
+            @"CREATE TABLE SendEmailCommand(
+                Id INTEGER PRIMARY KEY,
+                [To] VARCHAR(255) NOT NULL,
+                Subject VARCHAR(255) NOT NULL,
+                Body BLOB
+            );";
+
         public SqliteConnection Connection { get; }
 
         public SampleScmDataFixture()
@@ -28,6 +66,8 @@ namespace SqliteScmTest
 
             Connection.Open();
 
+            // Tables
+
             var command = new SqliteCommand(PartTypeTable, Connection);
             
             command.ExecuteNonQuery();
@@ -35,6 +75,24 @@ namespace SqliteScmTest
             command = new SqliteCommand(InventoryItemTable, Connection);
             
             command.ExecuteNonQuery();
+
+            command = new SqliteCommand(PartCommandTable, Connection);
+            
+            command.ExecuteNonQuery();
+
+            command = new SqliteCommand(SupplierTable, Connection);
+            
+            command.ExecuteNonQuery();
+
+            command = new SqliteCommand(OrderTable, Connection);
+            
+            command.ExecuteNonQuery();
+
+            command = new SqliteCommand(SendEmailCommandTable, Connection);
+            
+            command.ExecuteNonQuery();
+
+            // Data
 
             command = new SqliteCommand(
                 "INSERT INTO PartType (Name) VALUES ('8289 L-shaped plate');",
@@ -46,12 +104,19 @@ namespace SqliteScmTest
                 @"INSERT INTO InventoryItem
                     (PartTypeId, Count, OrderThreshold)
                     VALUES
-                    (1, 100, 10)"
-                , Connection);
+                    (1, 100, 10)", 
+                    Connection);
 
             command.ExecuteNonQuery();
 
-            // TODO: Add the table for part commands
+            command = new SqliteCommand(
+                @"INSERT INTO Supplier
+                (Name, Email, PartTypeId)
+                VALUES
+                ('Joe Supplier', 'joe@joesupplier.com', 0)",
+                Connection);
+            
+            command.ExecuteNonQuery();
         }
 
         public void Dispose()
