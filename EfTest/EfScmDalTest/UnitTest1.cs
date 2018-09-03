b464ae8e7c9e07a82f6dfc17a555bf8919a95584
Jorge Cabot <jcabot@jorgecabot.com>
2018-09-03 15:19:17 -0400

    Added passing test for db usage.

--- EfTest/EfScmDalTest/UnitTest1.cs
+++ EfTest/EfScmDalTest/UnitTest1.cs
@@ -1,4 +1,7 @@
+using EfScmDataAccess;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace EfScmDalTest
@@ -6,9 +9,30 @@ namespace EfScmDalTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task Test1()
         {
+            using (var context = new EfScmContext())
+            {
+                var partName = $"Sample {DateTime.Now.ToString("HHmmss")}";
+                var part = new PartType()
+                {
+                    Name = partName
+                };
 
+                await context.Parts.AddAsync(part);
+                await context.SaveChangesAsync();
+
+                var dbPart = context.Parts.Single(p => p.Name == partName);
+
+                Assert.Equal(dbPart.Name, part.Name);
+
+                context.Parts.Remove(dbPart);
+                await context.SaveChangesAsync();
+                
+                dbPart = context.Parts.FirstOrDefault(p => p.Name == partName);
+
+                Assert.Null(dbPart);
+            }
         }
     }
 }
